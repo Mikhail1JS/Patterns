b38174d7fbef0dbf787976e3d5a56639d416f8df
Mikhail1JS <misha.zaichko@gmail.com>
2023-01-14 00:40:06 +0500

    hw4

--- main.php
+++ main.php
@@ -1,63 +1,16 @@
 <?php
-interface RepositoryInterface {
-    public function getOne(): array;
-    public function getDb(): StorageInterface;
-}
 
-interface StorageInterface {
+$sqlFactory = new MySQLFactory();
+$postgresqlFactory = new PostgreSQLFactory();
+$oraclesqlFactory = new OracleSQLFactory();
 
-
-    public function query(): array;
-}
-
-class MysqlRepository implements RepositoryInterface {
-
-    public function getOne(): array
-    {
-        $db = $this->getDb();
-        return $db->query();
-    }
-
-    public function getDb(): StorageInterface {
-        return new MysqlStorage();
-    }
-}
-
-class RedisRepository implements RepositoryInterface {
-
-    public function getOne(): array
-    {
-        $db = $this->getDb();
-        return $db->query();
-    }
-
-    public function getAll(): array
-    {
-        $db = $this->getDb();
-        return $db->query();
-    }
-
-    public function getDb(): StorageInterface {
-        return new RedisStorage();
-    }
-}
-
-class MysqlStorage implements StorageInterface {
-
-    public function query(): array
-    {
-        return [];
-    }
-}
-
-
-
+$SmartOrm = new ORM($sqlFactory);
 
 class ORM {
 
     protected $connection;
-    protected $renderer;
-    protected $logger;
+    protected $record;
+    protected $query;
 
     public function __construct(ServiceFactoryInterface $serviceFactory)
     {
@@ -73,49 +26,242 @@ class ORM {
     }
 }
 
-abstract class AbstractConnection  {
 
-    public function __construct(string $connection)
-    {
-    }
+
+interface InterfaceConnection  {
+    public function getDB(): StorageInterface;
 
 };
-interface RendererInterface {};
-interface LoggerInterface {};
+
+class MySqlConnection implements InterfaceConnection
+{
+
+    public function __construct(public string|array $config){}
+    public function getDB():StorageInterface
+    {
+        $connection = new PDO ($this->config);
+
+        return new MySqlStorage($connection);
+    }
+
+}
+
+class PostgreSQLConnection implements InterfaceConnection {
+    public function __construct(public string|array $config){}
+    public function getDB():StorageInterface
+    {
+        $connection = new PDO ($this->config);
+
+       return new PostgreSqlStorage($connection);
+    }
+
+}
+
+class OracleSQLConnection implements InterfaceConnection {
+    public function __construct(public string|array $config){}
+    public function getDB():StorageInterface
+    {
+        $connection = new PDO ($this->config);
+
+        return new OracleSQLStorage($connection);
+    }
+
+}
+
+class DBQuery
+{
+    public $select;
+    public $where;
+}
+
+class DBQueryBuilder
+{
+    public function __construct( public DBQuery $query){}
+
+    public function select($field): self
+    {
+        $this->query->select = $field;
+        return $this;
+    }
+
+    public function condition($condition): self
+    {
+        $this->query->where = $condition;
+        return $this;
+    }
+
+    public function getCriteria(): DBQuery
+    {
+        return $this->query;
+    }
+}
 
 
-class MysqlConnection extends AbstractConnection {
+interface StorageInterface {
+    public function query(string $query);
+}
 
+class MySqlStorage implements StorageInterface {
+
+    public function __construct(private PDO $connection){
+
+    }
+
+    public function query(string $query): PDOStatement
+    {
+        return $this->connection->query($query);
+    }
+}
+
+class PostgreSqlStorage implements StorageInterface {
+
+    public function __construct(private PDO $connection){
+
+    }
+
+    public function query(string $query): PDOStatement
+    {
+        return $this->connection->query($query);
+    }
+}
+
+class OracleSqlStorage implements StorageInterface {
+
+    public function __construct(private PDO $connection){
+
+    }
+
+    public function query(string $query): PDOStatement
+    {
+        return $this->connection->query($query);
+    }
+}
+
+interface DBRecordInterface {
+    public function getOne(string $query);
+
+    public function getAll(string $query): array;
+
+
+}
+
+class MySqlDBRecord implements DBRecordInterface {
+
+    public function __construct (public StorageInterface $db ){}
+    public function getAll(string $query): array
+    {
+        return $this->db->query($query)->fetchAll();
+    }
+
+    public function getOne(string $query)
+    {
+        return $this->db->query($query)->fetch();
+    }
+}
+
+
+class PostgreSQLDBRecord implements DBRecordInterface {
+
+    public function __construct (public StorageInterface $db ){}
+    public function getAll(string $query): array
+    {
+        return $this->db->query($query)->fetchAll();
+    }
+
+    public function getOne(string $query)
+    {
+        return $this->db->query($query)->fetch();
+    }
+}
+
+
+class OracleSQLDBRecord implements DBRecordInterface {
+
+    public function __construct (public StorageInterface $db ){}
+    public function getAll(string $query): array
+    {
+        return $this->db->query($query)->fetchAll();
+    }
+
+    public function getOne(string $query)
+    {
+        return $this->db->query($query)->fetch();
+    }
 }
 
 
 
+interface ServiceFactoryInterface {
 
-
-
-abstract class ServiceFactoryInterface {
-
-    abstract public function DBConnection(string $connection): AbstractConnection;
-    abstract public function DBRecord(): RecordInterface;
-    abstract public function DBQueryBuilder(): QueryInterface;
+    public function DBConnection(): InterfaceConnection;
+    public function DBRecord(): DBRecordInterface;
+    public function DBQueryBuilder(): DBQueryBuilder;
 }
 
-class ProductionServiceFactory extends ServiceFactoryInterface {
+class MySQLFactory implements ServiceFactoryInterface {
 
-    public function DBConnection($string): ConnectionInterface
+
+    public function DBConnection():  InterfaceConnection
     {
-        return new MysqlConnection();
+        $connection = '';
+        return new MysqlConnection($connection);
     }
 
-    public function DBRecord(): RecordInterface;
+    public function DBRecord(): DBRecordInterface
     {
+        $db = $this->DBConnection()->getDB();
+        return new MySqlDBRecord($db);
 
     }
 
-    public function DBQueryBuilder(): QueryInterface
+    public function DBQueryBuilder(): DBQueryBuilder
     {
-
+        return new DBQueryBuilder(new DBQuery());
     }
 }
 
 
+class PostgreSQLFactory implements ServiceFactoryInterface {
+
+
+    public function DBConnection():  InterfaceConnection
+    {
+        $connection = '';
+        return new PostgreSQLConnection($connection);
+    }
+
+    public function DBRecord(): DBRecordInterface
+    {
+        $db = $this->DBConnection()->getDB();
+        return new PostgreSQLDBRecord($db);
+
+    }
+
+    public function DBQueryBuilder(): DBQueryBuilder
+    {
+        return new DBQueryBuilder(new DBQuery());
+    }
+}
+
+
+class OracleSQLFactory implements ServiceFactoryInterface {
+
+
+    public function DBConnection():  InterfaceConnection
+    {
+        $connection = '';
+        return new OracleSQLConnection($connection);
+    }
+
+    public function DBRecord(): DBRecordInterface
+    {
+        $db = $this->DBConnection()->getDB();
+        return new OracleSQLDBRecord($db);
+
+    }
+
+    public function DBQueryBuilder(): DBQueryBuilder
+    {
+        return new DBQueryBuilder(new DBQuery());
+    }
+}
